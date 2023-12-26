07baf426ed034533fff3268eaef219237ba02ce5
ZISHAN RAYEEN <136910246+zishanrayeen@users.noreply.github.com>
2023-12-26 20:39:02 +0530

    all file upload
    THIS APP IS CREATED BY A HTML, CSS AND JAVA SCRIPT.

---
+++ script.js
@@ -0,0 +1,41 @@
+
+
+//CODE TYPE FUNCTION DATA
+function zishan(){
+
+    var a = document.getElementById("coding").value;
+
+    var b = document.getElementById("final");
+    b.innerHTML=a;
+
+    var c = document.getElementById("editor");
+    c.style.display="none";
+
+    var d = document.getElementById("result");
+    d.style.display="block";
+    
+
+    var e = document.getElementById("run");
+    e.style.display="none";
+
+    var f = document.getElementById("back");
+    f.style.display="block";
+
+}
+
+
+//RETURN TYPE CODE FUNCTION 
+function sana(){
+    var a = document.getElementById("editor");
+    a.style.display="block";
+    
+    var b = document.getElementById("result");
+    b.style.display="none";
+
+    var c = document.getElementById("run");
+    c.style.display="block";
+
+    var d = document.getElementById("back");
+    d.style.display="none";
+}
+
